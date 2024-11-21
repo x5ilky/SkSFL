4044c1917a7e4735b5783575e15b78f7e4fec02a
x5ilky <ethancai920@gmail.com>
2024-11-21 15:17:06 +1100

    [SkTg] new type generator

--- tools/SkTg.ts
+++ tools/SkTg.ts
@@ -2,10 +2,10 @@
 /*
 --- Example Syntax ---
 
-renum ReportType<T> {
-    Foo { test: $[ string ]$ }
-    Bar { boo: $[ T ]$ } 
-}
+renum ReportType<T, E> do
+    Foo ${ test: string }$
+    Bar ${ boo: T }$
+end
 
 // generates
 
@@ -15,13 +15,13 @@ function match<T extends {__type: string, value: any}>(value: T, matchFor: { [k 
 }
 
 type ReportType<T> = 
-    ReportType$Foo | ReportType$Bar
+    ReportType$Foo<T> | ReportType$Bar<T>
 
-type ReportType$Foo = { __type: "Foo", value: {
+type ReportType$Foo<T> = { __type: "Foo", value: {
     test: string
 }};
-type ReportType$Bar = { __type: "Bar", value: {
-    test: string
+type ReportType$Bar<T> = { __type: "Bar", value: {
+    boo: T
 }}
 
 const ReportType$Foo = (value: ReportType$Foo["value"]) => { ... }
@@ -33,21 +33,71 @@ import { skap } from "../shared/SkAp.ts"
 const shape = skap.command({
     subc: skap.subcommand({
         build: skap.command({
-            inputFile: skap.string("-i").required(),
-            outputFile: skap.string("-o").required(),
+            inputFile: skap.string("-i").required().description("input path"),
+            outputFile: skap.string("-o").required().description("output path"),
+            doExport: skap.boolean("-e").description("whether to export types")
         })
     }).required()
 })
 
-const cmd = shape.parse(Deno.args);
+const cmd = shape.parse(Deno.args, {
+    customError: (e) => {
+        console.error(e);
+        Deno.exit(1);
+    }
+});
+
+const renumRegex = /renum\s+(\w+)(<?([\w\s,]+?)>?)\s+do(.*?)end/gms;
+const renumInner = /(\w+)\s+\${(.*?)}\$/gms;
 
 if (cmd.subc.selected === "build") {
     const {
         inputFile,
-        outputFile
+        outputFile,
+        doExport
     } = cmd.subc.commands.build!;
 
-    const input = await Deno.readFile(inputFile);
+    const exp = doExport ? "export " : "";
 
-    
+    let out = `
+// deno-lint-ignore no-explicit-any
+${exp}function match<T extends {__type: string, value: any}>(value: T, matchFor: { [k in T["__type"]]: (value: Extract<T, {__type: k}>["value"]) => void } ) {
+    if (value.__type in matchFor) {
+        matchFor[value.__type as T["__type"]](value.value)
+    }
+}
+`;
+
+    const input = await Deno.readTextFile(inputFile);
+
+    for (const renumMatch of input.matchAll(renumRegex)) {
+        const [
+            _total,
+            name,
+            genericTotal,
+            _generics,
+            body
+        ] = renumMatch;
+
+        let totalType = `${exp}type ${name}${genericTotal} = `;
+        const types = [];
+        
+        for (const inner of body.matchAll(renumInner)) {
+            const [
+                _total,
+                ename,
+                ebody
+            ] = inner;
+
+            out += `${exp}type ${name}$${ename}${genericTotal} = { __type: ${JSON.stringify(ename)}, value: {${ebody}}};\n`;
+            out += `${exp}const ${name}$${ename} = ${genericTotal}(value: ${name}$${ename}${genericTotal}["value"]): ${name}${genericTotal} => { return {__type: ${JSON.stringify(ename)}, value} };\n`
+            types.push(`${name}$${ename}${genericTotal}`);
+
+        }
+        totalType += types.join(" | ");
+        totalType += ";\n"
+        out += totalType;
+    }
+
+    await Deno.writeTextFile(outputFile, out);
 }
