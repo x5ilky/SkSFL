1d88655e7d22bb0432f02a9f3fe4be3a04796d52
x5ilky <ethancai920@gmail.com>
2024-11-27 14:18:56 +1100

    move

--- shared/SkAn.ts
+++ shared/SkAn.ts
@@ -48,54 +48,53 @@ export const Ansi = {
     // 24-bit
     rgb: (r: number, g: number, b: number) => `\x1b[38;2;${r};${g};${b}m`,
     bgRgb: (r: number, g: number, b: number) => `\x1b[48;2;${r};${g};${b}m`,
-    
+
     rgbHex: (_hex: string) => "",
     bgRgbHex: (_hex: string) => "",
 
     cursor: {
         // Move the cursor up by n lines
         moveUp: (n: number) => `\x1b[${n}A`,
-    
+
         // Move the cursor down by n lines
         moveDown: (n: number) => `\x1b[${n}B`,
-    
+
         // Move the cursor forward by n columns
         moveForward: (n: number) => `\x1b[${n}C`,
-    
+
         // Move the cursor backward by n columns
         moveBackward: (n: number) => `\x1b[${n}D`,
-    
+
         // Move the cursor to a specific position (row, column)
         moveTo: (row: number, col: number) => `\x1b[${row};${col}H`,
-    
+
         // Save the current cursor position
         savePosition: () => `\x1b[s`,
-    
+
         // Restore the saved cursor position
         restorePosition: () => `\x1b[u`,
-    
-        // Clear the screen and move the cursor to the top-left
-        clearScreen: () => `\x1b[2J`,
-    
-        // Clear the current line from the cursor to the end
-        clearLineToEnd: () => `\x1b[0K`,
-    
-        // Clear the current line from the cursor to the beginning
-        clearLineToStart: () => `\x1b[1K`,
-    
-        // Clear the entire current line
-        clearLine: () => `\x1b[2K`,
-    }
-}
+    },
+    // Clear the screen and move the cursor to the top-left
+    clearScreen: () => `\x1b[2J`,
+
+    // Clear the current line from the cursor to the end
+    clearLineToEnd: () => `\x1b[0K`,
+
+    // Clear the current line from the cursor to the beginning
+    clearLineToStart: () => `\x1b[1K`,
+
+    // Clear the entire current line
+    clearLine: () => `\x1b[2K`,
+};
 
 Ansi.rgbHex = (hex: string) => Ansi.rgb(...hexToRgb(hex));
 Ansi.bgRgbHex = (hex: string) => Ansi.bgRgb(...hexToRgb(hex));
 
 function hexToRgb(hex: string) {
     const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)!;
-    return <const>[
+    return [
         parseInt(result[1], 16),
         parseInt(result[2], 16),
-        parseInt(result[3], 16)
-    ]
+        parseInt(result[3], 16),
+    ] as const;
 }
