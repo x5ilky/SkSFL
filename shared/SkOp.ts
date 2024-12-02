8cc15a6d248929571fd54e4054427f7cf9494b44
x5ilky <ethancai920@gmail.com>
2024-12-02 14:16:04 +1100

    more skop

--- shared/SkOp.ts
+++ shared/SkOp.ts
@@ -1,36 +1,78 @@
 
-export class SkOption<T> {
-  __v: TOption<T>;
-  constructor(v: TOption<T>) {
+export abstract class SkError {
+  public get stack() : string | undefined {
+    const err = new Error();
+    return err.stack;
+  };
+  public abstract get name() : string;
+  public abstract get description() : string;
+}
+
+export class SkOptionError extends SkError {
+  public override get name(): string {
+    return "SkOptionError"
+  }
+
+  public override get description(): string {
+    return "Tried to unwrap SkOption<T>"
+  }
+}
+export class SkResult<T, E extends SkError> {
+  __v: TResult<T, E>;
+  constructor(v: TResult<T, E>) {
     this.__v = v;
   }
 
-  is_some(): this is { __v: Some<T> } {
+  is_ok(): this is { __v: Ok<T> } {
     return this.__v.__op;
   }
 
-  is_none(): this is { __v: None } {
+  is_ok_and(pred: (v: T) => boolean): this is { __v: Ok<T> } {
+    if (this.is_ok() && pred(this.unwrap())) return true;
+    return false;
+  }
+
+  is_err(): this is { __v: Err<E> } {
     return !this.__v.__op;
   }
 
+  is_err_and(pred: (v: E) => boolean): this is { __v: Err<E> } {
+    if (this.is_err() && pred(this.unwrap_err())) return true;
+    return false;
+  }
+
   unwrap(): T {
-    return this.expect('Unwrap failed on Option<T>');
+    if (this.is_err()) {
+      throw this.__v.val;
+    }
+    if (this.is_ok()) return this.__v.val;
+    throw new Error("Unreachable");
   }
 
   unwrap_or(v: T): T {
-    return this.is_some() ? this.__v.val : v;
+    return this.is_ok() ? this.__v.val : v;
+  }
+
+  unwrap_err(): E {
+    if (this.is_ok()) {
+      throw new Error("got ok value on unwrap_err");
+    }
+    if (this.is_err()) return this.__v.val;
+    throw new Error("Unreachable");
   }
 
   expect(msg: string): T {
-    if (this.is_none()) {
+    if (this.is_err()) {
+      this
       throw new Error(msg);
-    } else {
-      return this.__v.val!;
+    } else if (this.is_ok()) {
+      return this.__v.val;
     }
+    throw new Error("Unreachable");
   }
 
   run_if_some(cb: (v: T) => void): boolean {
-    if (this.is_some()) {
+    if (this.is_ok()) {
       cb(this.unwrap());
       return true;
     } else {
@@ -38,11 +80,32 @@ export class SkOption<T> {
     }
   }
 }
+export class SkOption<T> extends SkResult<T, SkOptionError> {
+  constructor(value: TOption<T>) {
+    super(value);
+  }
 
-export type Some<T> = { __op: true; val: T };
-export const makesome = <T>(val: T): Some<T> => ({ __op: true, val });
-export const Some = <T>(val: T): SkOption<T> => new SkOption(makesome(val));
-export type None = { __op: false; val: null };
-export const makenone = <T>(): None => ({ __op: false, val: null });
-export const None = <T>(): SkOption<T> => new SkOption<T>(makenone());
-export type TOption<T> = Some<T> | None;
+  is_some(): this is {__v: Ok<T>} {
+    return this.is_ok()
+  }
+  is_some_and(pred: (v: T) => boolean): this is {__v: Ok<T>} {
+    return this.is_ok_and(pred);
+  }
+  is_none(): this is {__v: Err<SkOptionError>} {
+    return this.is_err()
+  }
+}
+
+export type Ok<T> = { __op: true; val: T };
+const makeok = <T>(val: T): Ok<T> => ({ __op: true, val });
+export const Ok = <T, E extends SkError>(val: T): SkResult<T, E> => new SkResult<T, E>(makeok(val));
+export type Err<E extends SkError> = { __op: false; val: E };
+const makeerr = <E extends SkError>(error: E): Err<E> => ({ __op: false, val: error });
+export const Err = <T, E extends SkError>(error: E): SkResult<T, E> => new SkResult<T, E>(makeerr(error));
+
+export type Some<T> = Ok<T>;
+export type None = Err<SkOptionError>;
+export const Some = <T>(val: T) => new SkOption<T>({__op: true, val});
+export const None = <T>() => new SkOption<T>({__op: false, val: new SkOptionError()});
+type TOption<T> = Some<T> | None;
+type TResult<T, E extends SkError> = Ok<T> | Err<E>;
