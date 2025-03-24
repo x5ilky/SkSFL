afc5a582b97cae7d77f24cd2dbae9965d81fd0c3
5ilky <ethancai920@gmail.com>
2025-03-24 17:25:32 +1100

    database

--- shared/SkDb.ts
+++ shared/SkDb.ts
@@ -3,29 +3,43 @@
 
 // #begin_import
 import { skfs } from "../deno/SkAgnostic.ts";
+import { Logger } from "./SkLg.ts";
 import { SkSerializer } from "./SkSr.ts";
 // #end_import
 
 export class Database<T> {
     value: T;
-    constructor(private path: string, private initialValue?: T) {
+    constructor(private path: string, private options: {
+        initialValue?: T,
+        logger?: Logger
+    }) {
         this.value = null as unknown as T;
         this.load();
         skfs.onExit(async () => {
-            const ser = new SkSerializer();
-            await skfs.writeTextFile(path, ser.serialize(this.value));
+            await this.write.bind(this)();
         });
+        setInterval(async () => {
+            await this.write();
+        }, 1000*60*10);
     }
 
+    async write() {
+        const ser = new SkSerializer();
+        await skfs.writeTextFile(this.path, ser.serialize(this.value));
+        if (this.options.logger !== undefined) this.options.logger.info(`Wrote data to database file`)
+    }
     load() {
         try {
+            if (this.options.logger !== undefined) this.options.logger.info(`Trying to load`)
             const ser = new SkSerializer();
             this.value = ser.deserialize(skfs.readTextFileSync(this.path));
+            if (this.options.logger !== undefined) this.options.logger.info(`Loaded database data`)
         } catch {
-            if (this.initialValue === undefined) throw new Error(`No initial value found and database is aso not found`)
-            this.value = this.initialValue;
+            if (this.options.initialValue === undefined) throw new Error(`No initial value found and database is aso not found`)
+            this.value = this.options.initialValue;
             const ser = new SkSerializer();
             skfs.writeTextFileSync(this.path, ser.serialize(this.value));
+            if (this.options.logger !== undefined) this.options.logger.info(`Failed to read database file, creating file with default value`)
         }
     }
 }
