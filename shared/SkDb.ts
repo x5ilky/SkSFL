88f176d3bd539d47ec289c84999991c74e5430f6
5ilky <ethancai920@gmail.com>
2025-06-06 19:39:52 +1000

    let skdb take optinal custom

--- shared/SkDb.ts
+++ shared/SkDb.ts
@@ -13,7 +13,7 @@ export class Database<T> {
         initialValue?: T,
         logger?: Logger,
         timer?: number
-    }) {
+    }, private custom?: (v: any) => any) {
         this.value = null as unknown as T;
         this.load();
         skfs.onExit(async () => {
@@ -26,20 +26,20 @@ export class Database<T> {
 
     async write() {
         const ser = new SkSerializer();
-        await skfs.writeBinary(this.path, ser.serialize(this.value));
+        await skfs.writeBinary(this.path, ser.serialize(this.value, this.custom?.bind(this)));
         if (this.options.logger !== undefined) this.options.logger.info(`Wrote data to database file`)
     }
     load() {
         try {
             if (this.options.logger !== undefined) this.options.logger.info(`Trying to load`)
             const ser = new SkSerializer();
-            this.value = ser.deserialize(skfs.readBinarySync(this.path));
+            this.value = ser.deserialize(skfs.readBinarySync(this.path), this.custom?.bind(this));
             if (this.options.logger !== undefined) this.options.logger.info(`Loaded database data`)
         } catch {
             if (this.options.initialValue === undefined) throw new Error(`No initial value found and database is aso not found`)
             this.value = this.options.initialValue;
             const ser = new SkSerializer();
-            skfs.writeBinarySync(this.path, ser.serialize(this.value));
+            skfs.writeBinarySync(this.path, ser.serialize(this.value, this.custom?.bind(this)));
             if (this.options.logger !== undefined) this.options.logger.info(`Failed to read database file, creating file with default value`)
         }
     }
